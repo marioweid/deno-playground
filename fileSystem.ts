af5cad8a686a33b2c3a4473b3c6ba1ceb350e73a
S4ltyGo4t <mario.weidner@gmx.de>
2020-05-15 16:07:10 +0200

    added unstable information for file system usage

--- fileSystem.ts
+++ fileSystem.ts
@@ -3,4 +3,7 @@ import * as path from "https://deno.land/std/path/mod.ts";
 
 const testPath = path.join("./", "foo");
 ensureDir(testPath); // returns promise
-// not working , missing tpes
+
+// important
+// requres --unstable flag
+// deno run --unstable --allow-read=./ --allow-write=./ fileSystem.ts
